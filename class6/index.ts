252b518b48ff8b187fdcd3daa5e3e3204297ce32
talhak911 <talhakhan8300@gmail.com>
2023-09-05 12:58:53 +0500

    class6 added

---
+++ class6/index.ts
@@ -0,0 +1,27 @@
+type studentType={
+    name: string;
+    age: number;
+    class: number;
+    isActive: boolean;
+}
+let Students:studentType[]=[
+{   name :"Talha",
+    age:2,
+    class:3,
+    isActive:true
+},
+{   name :"Ali",
+    age:16,
+    class:10,
+    isActive:true},
+{
+    name :"Javed",
+    age:13,
+    class:7,
+    isActive:false
+}]
+
+
+
+
+
