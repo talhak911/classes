cf7b3858cc57ebef56db0b684d58eac8b2d21078
talhak911 <talhakhan8300@gmail.com>
2023-09-05 13:00:56 +0500

    class6 added

---
+++ class6/index.js
@@ -0,0 +1,18 @@
+"use strict";
+let Students = [
+    { name: "Talha",
+        age: 2,
+        class: 3,
+        isActive: true
+    },
+    { name: "Ali",
+        age: 16,
+        class: 10,
+        isActive: true },
+    {
+        name: "Javed",
+        age: 13,
+        class: 7,
+        isActive: false
+    }
+];
