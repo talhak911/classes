30dc32e6cc06dec33f651d12e56ec48532c1a314
talhak911 <talhakhan8300@gmail.com>
2023-09-21 22:08:11 +0500

    Maxima algorithm

--- class10/index.js
+++ class10/index.js
@@ -4,3 +4,36 @@ for (let i = 0; i < data.length; i++) {
     console.log("Name is ", element.name);
     console.log("ID is ", element.id);
 }
+class point {
+    x;
+    y;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+function maxima(n, P) {
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        let maximal = true;
+        for (let j = 0; j < n; j++) {
+            if (i !== j && P[i].x <= P[j].x && P[i].y <= P[j].y) {
+                maximal = false;
+                break;
+            }
+        }
+        if (maximal === true) {
+            result.push(P[i]);
+        }
+    }
+    return result;
+}
+const points = [
+    new point(200, 300),
+    new point(100, 500),
+    new point(150, 400),
+    new point(300, 200),
+    new point(250, 350)
+];
+const result = maxima(points.length, points);
+console.log(result);
--- class10/index.ts
+++ class10/index.ts
@@ -5,3 +5,45 @@ for (let i=0;i<data.length;i++){
     console.log("ID is ", element.id);
     
 }
+
+class point{
+    constructor(public x:number,public y:number){}
+}
+
+function maxima (n:number,P:point[]):point[]{
+    const result:point[]=[];
+    for (let i = 0; i < n; i++) {
+        let maximal = true;
+
+        for (let j = 0; j < n; j++) {
+            if (i !== j && P[i].x <= P[j].x && P[i].y <= P[j].y) {
+                maximal = false;
+                break;
+            }
+        }
+
+        if (maximal === true) {
+            result.push(P[i]);
+        }
+    }
+
+    return result;
+}
+
+
+const points: point[] = [
+    new point(200, 300),
+    new point(100, 500),
+    new point(150, 400),
+    new point(300, 200),
+    new point(250, 350),
+    new point(300, 200),
+    new point(200, 100),
+    new point(350, 500),
+    new point(600, 100)
+];
+
+const result = maxima(points.length, points);
+console.log(result);
+
+
