24ba0607dfde53d79d72df75656a16830afdd1ca
talhak911 <talhakhan8300@gmail.com>
2023-12-02 18:57:18 +0500

    Find prime number

--- class 2/class2.js
+++ class 2/class2.js
@@ -1,1 +1,12 @@
 "use strict";
+function findNumber(a) {
+    for (let i = 2; i < a; i++) {
+        if (a % i == 0) {
+            return ("It is not prime number");
+        }
+        else {
+            return ("it is prime ");
+        }
+    }
+}
+console.log(findNumber(7));
---
+++ class 2/class2.ts
@@ -0,0 +1,11 @@
+function findNumber(a:number){
+    for (let i =2;i<a ;i++){
+        if(a % i ==0){
+            return("It is not prime number")}
+            else {
+                return("it is prime ")
+            }
+    }
+    
+    }
+console.log(findNumber(7))
